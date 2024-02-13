21bf726a0474e8659e9f0e65c11ad1af62a61007
«Lif» <ysalissou@gmail.com>
2024-02-13 17:28:05 +0000

    initial commint

---
+++ src/main/java/com/technolab/keneyaso/KeneyasoApplication.java
@@ -0,0 +1,13 @@
+package com.technolab.keneyaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeneyasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeneyasoApplication.class, args);
+	}
+
+}
