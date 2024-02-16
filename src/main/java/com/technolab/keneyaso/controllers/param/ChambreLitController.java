f1c877bb4a858d05a991599e86ae673aac329ea2
Mohamed ivan doumbia <mdoumb@hotmail.fr>
2024-02-16 00:32:39 +0000

    Création de Chambre controlleur avec methode GET

---
+++ src/main/java/com/technolab/keneyaso/controllers/param/ChambreLitController.java
@@ -0,0 +1,18 @@
+package com.technolab.keneyaso.controllers.param;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/chambre")
+public class ChambreLitController {
+
+    @GetMapping("/find")
+    public ResponseEntity findAll(){
+        return ResponseEntity.ok("Liste des chambres");
+    }
+    @GetMapping("{id}")
+    public ResponseEntity findOne(@PathVariable long id){
+        return ResponseEntity.ok("Liste des chambre avec l'id: "+ id);
+    }
+}
